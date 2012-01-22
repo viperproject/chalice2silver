c34d94a1d4c0c1aeb67fad74c1200fcc8e624a33
Christian Klauser <klauserc@ethz.ch>
2012-01-22 20:00:51 +0100

    Use scopt as dependency. Add scalatest dependency. Use silast and chalice as unmanaged dependencies.

--- chalice2sil/build.sbt
+++ chalice2sil/build.sbt
@@ -1,6 +1,11 @@
+// Chalice2SIL
+
 name := "chalice2sil"
 
 version := "0.1"
 
-scalaVersion := "2.9.1"
+scalaVersion := "2.8.1"
 
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.+" % "test"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "1.1.+"
---
+++ update-libs.ps1
@@ -0,0 +1,41 @@
+param([Switch] $SkipCompile)
+
+$proj_root = "C:\Users\Christian\ETH\HS11\Bachelor";
+$chalice_root = Join-Path $proj_root Boogie\Chalice;
+$silast_root  = Join-Path $proj_root Chalice2SIL\silast;
+
+if($SkipCompile){
+    $compileCmd = "";
+}else{
+    $compileCmd = "compile";
+}
+
+$sd = Get-ScriptDirectory;
+echo $sd;
+
+$sbt = Join-Path $sd "chalice2sil\sbt.ps1";
+
+Push-Location $chalice_root
+    echo "================== COMPILING CHALICE =============================="
+    & $sbt $compileCmd package
+    echo "================== DONE COMPILING CHALICE ========================="
+Pop-Location
+
+Push-Location $silast_root
+    echo "================== COMPILING SILAST ==============================="
+    & $sbt $compileCmd package
+    echo "================== DONE COMPILING SILAST =========================="
+Pop-Location
+
+$chalice_jar = Join-Path $chalice_root target\scala-2.8.1\chalice_2.8.1-1.0.jar
+$silast_jar  = Join-Path $silast_root  target\scala-2.8.1\silast_2.8.1-0.1.jar
+
+$lib_dir = "chalice2sil\lib";
+
+if(-not (Test-Path $lib_dir)){
+    New-Item -Type Container -Path $lib_dir | Out-Null;
+}
+
+cp $CHALICE_JAR (Join-Path $lib_dir "chalice.jar")
+cp $SILAST_JAR  (Join-Path $lib_dir "silast.jar")
+
