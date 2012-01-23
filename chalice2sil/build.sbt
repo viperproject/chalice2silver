c9091c86a7e28921f4b8f50dc368d3ec71db7c6c
Christian Klauser <klauserc@ethz.ch>
2012-01-23 13:06:36 +0100

    Quick-and-dirty project. (Scala 2.9.1)

--- chalice2sil/build.sbt
+++ chalice2sil/build.sbt
@@ -2,10 +2,14 @@
 
 name := "chalice2sil"
 
+organization := "ch.ethz.inf.pm.semper"
+
 version := "0.1"
 
-scalaVersion := "2.8.1"
+scalaVersion := "2.9.1"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "1.+" % "test"
+
+libraryDependencies += "junit" % "junit" % "4.+" % "test"
 
 libraryDependencies += "com.github.scopt" %% "scopt" % "1.1.+"
--- update-libs.ps1
+++ update-libs.ps1
@@ -2,7 +2,7 @@ param([Switch] $SkipCompile)
 
 $proj_root = "C:\Users\Christian\ETH\HS11\Bachelor";
 $chalice_root = Join-Path $proj_root Boogie\Chalice;
-$silast_root  = Join-Path $proj_root Chalice2SIL\silast;
+$silast_root  = Join-Path $proj_root Chalice2SIL\silast\src\SILAST;
 
 if($SkipCompile){
     $compileCmd = "";
@@ -17,7 +17,7 @@ $sbt = Join-Path $sd "chalice2sil\sbt.ps1";
 
 Push-Location $chalice_root
     echo "================== COMPILING CHALICE =============================="
-    & $sbt $compileCmd package
+    & $sbt "set scalaVersion := \`"2.9.1\`"" $compileCmd package #scala version needs to be escaped twice
     echo "================== DONE COMPILING CHALICE ========================="
 Pop-Location
 
@@ -27,8 +27,8 @@ Push-Location $silast_root
     echo "================== DONE COMPILING SILAST =========================="
 Pop-Location
 
-$chalice_jar = Join-Path $chalice_root target\scala-2.8.1\chalice_2.8.1-1.0.jar
-$silast_jar  = Join-Path $silast_root  target\scala-2.8.1\silast_2.8.1-0.1.jar
+$chalice_jar = Join-Path $chalice_root target\scala-2.9.1\chalice_2.9.1-1.0.jar
+$silast_jar  = Join-Path $silast_root  target\scala-2.9.1\silast_2.9.1-0.1.jar
 
 $lib_dir = "chalice2sil\lib";
 
