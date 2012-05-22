ab0e9def20d59f3e0531b58d87fa9dc123fa178b
Christian Klauser <klauserc@ethz.ch>
2012-05-22 09:21:37 +0200

    Add -Force switch to setup-repositories.ps1

--- chalice2sil/setup-repositories.ps1
+++ chalice2sil/setup-repositories.ps1
@@ -1,3 +1,5 @@
+param([Switch] $Force)
+
 # Chalice2SIL does not stand alone. 
 # It builds on Chalice (1) to create a SILAST (2) 
 # that it can then feed to Silicon (3) for verification.
@@ -13,8 +15,17 @@ $ErrorActionPreference = 'Stop';
 $Invocation = (Get-Variable MyInvocation -Scope 0).Value;
 $sd = (Split-Path $Invocation.MyCommand.Path);
 
-# Clone Boogie (Chalice is part of that repository)
+# Assemble paths
 $boogie = Join-Path $sd "boogie";
+$silast = Join-Path $sd "silast";
+$silicon = Join-Path $sd "silicon";
+
+if($Force){
+	Remove-Item $boogie,$silast,$silicon -Recurse -Force;
+}
+
+# Clone Boogie (Chalice is part of that repository)
+
 if(-not (Test-Path $boogie)){
     "Cloning Boogie to $boogie"
     hg clone https://hg.codeplex.com/boogie "$boogie" --quiet
@@ -26,19 +37,30 @@ if(-not (Test-Path $boogie)){
     cp $sbtjar,$sbt $chalice;
 }
 
-
-
 # Check out SILAST
-$silast = Join-Path $sd "silast";
+
 if(-not (Test-Path $silast)){
     "Checking out SILAST to $silast"
     svn checkout https://svn.inf.ethz.ch/svn/pmueller/pm-group/projects/semper/SILAST/trunk "$silast" --quiet
 }
 
 # Check out Silicon
-$silicon = Join-Path $sd "silicon";
+
 if(-not (Test-Path silicon)){
     "Checking out Silicon to $silicon"
     svn checkout https://svn.inf.ethz.ch/svn/pmueller/pm-group/projects/semper/SymbExEngine/trunk/Silicon "$silicon" --quiet
 }
+
+#########################################################################################
+########     TROUBLESHOOTING                                         ####################
+#########################################################################################
+#
+# 1. What does 'scala.tools.nsc.MissingRequirementError: object scala not found.' mean?
+#
+# The, or rather 'a', scala library is missing. This might be due to an old copy 
+# of silast.jar or chalice.jar in a /lib directory. Make sure that SBT manages the
+# inter-dependencies, i.e., delete silast.jar and chalice.jar from /lib.
+#
+#
+#
 
