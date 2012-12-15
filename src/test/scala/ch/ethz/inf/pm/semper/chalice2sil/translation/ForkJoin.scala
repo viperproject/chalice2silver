fa5d9ffccc5f429f34260e35618eb8f1f4f1ec0d
Stefan Heule <stefanheule@gmail.com>
2012-12-15 11:17:10 +0100

    (Remove unused import).

--- src/test/scala/ch/ethz/inf/pm/semper/chalice2sil/translation/ForkJoin.scala
+++ src/test/scala/ch/ethz/inf/pm/semper/chalice2sil/translation/ForkJoin.scala
@@ -1,6 +1,5 @@
 package ch.ethz.inf.pm.semper.chalice2sil.translation
 
-import org.scalatest.junit.JUnitRunner
 import ch.ethz.inf.pm.semper.chalice2sil.ChaliceSuite
 import org.scalatest.matchers.ShouldMatchers
 
