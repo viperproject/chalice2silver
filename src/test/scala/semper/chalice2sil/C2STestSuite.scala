6ef65e4ca0137cfdcf7878fe25e371a771b1507d
yannis <yannis@erde.inf.ethz.ch>
2014-03-07 13:35:03 +0100

    Changing the testing procedure to comply with the semper standard Currently all changes are commented out in C2STestSuite.scala

--- src/test/scala/semper/chalice2sil/C2STestSuite.scala
+++ src/test/scala/semper/chalice2sil/C2STestSuite.scala
@@ -10,6 +10,61 @@ import java.io.File
 import translation.ProgramTranslator
 import chalice.Chalice
 
+/*
+import semper.sil.testing.DefaultSilSuite
+import semper.sil.verifier.Verifier
+import semper.silicon.Silicon
+import semper.sil.frontend._
+import java.io.File
+import io.Source
+import java.nio.file.Path
+
+class Chalice2SILFrontEnd extends FrontEnd {
+  val phases = List(
+    Phase("parse and resolve", parseNResolve),
+    Phase("translate", translate),
+    Phase("verify", verify))
+
+  var verf: Verifier = null
+
+  var file: Path = null
+
+  override def init(verifier: Verifier) { verf = verifier }
+
+  override def reset(files: Seq[Path]) { file = files(0) }
+
+  override def parseNResolve() ...
+
+  override def translate() ...
+
+  override def verify() ...
+}
+
+class AllTests extends DefaultSilSuite {
+
+  override lazy val testDirectories: Seq[String] = Seq(
+    "basic",
+    "oldC2SCases",
+    "chaliceSuite/examples",
+    "chaliceSuite/general-tests",
+    "chaliceSuite/permission-model",
+    "chaliceSuite/predicates",
+    "chaliceSuite/regressions",
+    "chaliceSuite/substantial-examples",
+    "quantificationOverPermissions"
+  ).map("src/test/resources/" + _)
+
+  override def frontend(verifier: Verifier, files: Seq[Path]): Frontend = {
+    val fe = new Chalice2SILFrontEnd()
+    fe.init(verifier)
+    fe.reset(files.head)
+    fe
+  }
+
+  override def verifiers: Seq[Verifier] = Seq(new Silicon())
+}
+*/
+
 /**
     Author: Yannis Kassios
  */
