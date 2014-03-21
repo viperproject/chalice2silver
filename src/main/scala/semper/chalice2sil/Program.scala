2d2afa61126a7f5157dcca12838e0e35c234c2be
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2014-03-21 15:45:25 +0100

    Added possibility to write generated SIL program to a file instead of to stdio

--- src/main/scala/semper/chalice2sil/Program.scala
+++ src/main/scala/semper/chalice2sil/Program.scala
@@ -5,17 +5,18 @@
 package semper.chalice2sil
 
 import scopt._
-import chalice.{Chalice, PrintProgram}
+import chalice.Chalice
 import translation.ProgramTranslator
-import java.io.File
+import java.io.{FileWriter, PrintWriter, File}
 
 // **
 // Options to pass to the Chalice library; together with an optional filename
 // This translation parameter is currently not used, except to pass the -noVerify flag.
 // It may be removed in the future, unless a need to pass options to Chalice shows up.
 // **
-case class ProgramOptions(chaliceOptions : Map[String, String] = Map[String, String](),
-                          chaliceFile : java.io.File = null,
+case class ProgramOptions(chaliceOptions: Map[String, String] = Map[String, String](),
+                          chaliceFile: File = null,
+                          silFile: File = null,
                           showVersion: Boolean = false)
 
 // **
@@ -64,14 +65,17 @@ object Program {
   // Main
   // **
   def main(args: Array[String]) {
-    var chaliceFileName : String = null
-
     // Option parser
     val cmdParser = new OptionParser[ProgramOptions]("chalice2sil") {
       // Chalice file
       arg[File]("<chalice-file>") optional() action {
-        (source, c) => chaliceFileName = source.toString ; c.copy(chaliceFile = source)
+        (source, c) => c.copy(chaliceFile = source)
       } text ("The chalice source file.")
+
+      arg[File]("<sil-file>")
+        .optional()
+        .action{(dest, c) => c.copy(silFile = dest)}
+        .text("The SIL output file. If ommitted, results will be sent to stdio.")
 
       // Options for Chalice
       opt[(String, String)]("chop") action {
@@ -106,19 +110,39 @@ object Program {
       // we don't want Chalice to call Boogie
 
     // Translate
-    val chaliceProg = invokeChalice(progOptions)
-    if (chaliceProg == None) return
-    val chaliceProg1 = chaliceProg match { case Some(c) => c }
-    val (silProgram, messages) = new ProgramTranslator(chaliceFileName).translate(chaliceProg1)
+    val chaliceProg = invokeChalice(progOptions) match {
+      case Some(c) => c
+      case None => return
+    }
+
+    val (silProgram, messages) =
+      new ProgramTranslator(progOptions.chaliceFile.toString).translate(chaliceProg)
+
     def pluralize(noun: String, count: Int) = count match {
       case 0 => "no " + noun + "s"
       case 1 => "1 " + noun
       case n => n.toString + " " + noun + "s"
     }
 
-    // Report
-    Console.out.println(silProgram)
-    messages.foreach(m => Console.err.println("[Chalice2SIL] " + m))
-    Console.err.println("Chalice2SIL produced %s.".format(pluralize("message", messages.length)))
+    /* Report translation messages */
+
+    if (messages.nonEmpty) {
+      messages.foreach(m => Console.err.println("[Chalice2SIL] " + m))
+      Console.err.println("Chalice2SIL produced %s.".format(pluralize("message", messages.length)))
+    }
+
+    /* Write generated SIL program */
+
+    val sink = progOptions.silFile match {
+      case null => new PrintWriter(Console.out, true)
+      case file => new PrintWriter(new FileWriter(file))
+    }
+
+    sink.println(silProgram)
+
+    if (progOptions.silFile != null) {
+      sink.close()
+      println(s"Wrote generated SIL program to ${progOptions.silFile}")
+    }
   }
 }
